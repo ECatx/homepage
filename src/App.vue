88ee159ac89c0678fb0a152557a87d6faf9fc235
Eric Castellanos <eric@ericcastellanos.com>
2021-04-26 20:48:34 -0500

    Basic setup

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="main">
-    <div class="title">
-      <mdi:controller-classic-outline />
-      <h1 class="">Hello There</h1>
+    <div class="title flex-col">
+      <h1 class="mb-6 animate-pulse">Cat's With Hats Homepage</h1>
+      <fxemoji:tophat class="text-7xl animate-bounce"/>
+      <fxemoji:cat class="text-9xl"/>
+      <h1 class=""></h1>
     </div>
-    <p class="text-xl">Mouse is on {{ x }} and {{ y }}</p>
+    <div class="space-x-2">
+      <a href="https://final.catswithhats.online"><button class="bg-purple-900 text-white py-3 px-3 rounded-full shadow-lg hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-purple-400 ">Final</button></a>
+      <a href="https://chat.catswithhats.online"><button class="bg-blue-900 text-white py-3 px-3 rounded-full shadow-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-purple-400">Chat App</button></a>
+      <a href="https://notes.catswithhats.online"><button class="bg-green-900 text-white py-3 px-3 rounded-full shadow-lg hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-purple-400">Notes</button></a>
+    </div>
   </div>
 </template>
 
